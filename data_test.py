abeb8f95fc4c7011ecbf4dc5193830439488f9aa
maagic6 <mmr091600@gmail.com>
2024-01-08 11:40:07 +0000

    commit

--- data_test.py
+++ data_test.py
@@ -4,10 +4,6 @@ from rainy_dataset import RainyDataset
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 
-''' load MNIST dataset
-data_path = 'mnist/'
-dataset = torchvision.datasets.MNIST(root=data_path, train=True, download=False, transform=torchvision.transforms.ToTensor())'''
-
 dataset = RainyDataset(csv_file='None', root_dir='None', transform= transforms.ToTensor())
 
 # create DataLoader
