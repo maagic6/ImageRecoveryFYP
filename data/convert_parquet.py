69e023337e9ae2bfaf07efa44ddb03eed3e35f77
maagic6 <mmr091600@gmail.com>
2024-02-13 17:45:19 +0800

    parquet

--- data/convert_parquet.py
+++ data/convert_parquet.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+import io
 from datasets import Features, Image, Value
 
 features = Features({"before": Image(), "instruction": Value("string"), "after": Image()})
@@ -23,4 +24,4 @@ def convert_csv_to_parquet(input_file_path, output_file_path, drop_option, schem
     df = table.to_pandas()
     print(df.head())
 
-convert_csv_to_parquet('output.csv', 'data.parquet', 'row', schema)
+convert_csv_to_parquet('data.csv', 'data.parquet', 'row', schema)
