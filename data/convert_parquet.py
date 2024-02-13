c32d5654218e94457a601c540f4842b8aceba5da
maagic6 <mmr091600@gmail.com>
2024-02-13 17:27:43 +0800

    parquet

--- data/convert_parquet.py
+++ data/convert_parquet.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-import base64
+from datasets import Features, Image, Value
 
-def convert_csv_to_parquet(input_file_path, output_file_path, drop_option):
+features = Features({"before": Image(), "instruction": Value("string"), "after": Image()})
+schema = features.arrow_schema
+
+def convert_csv_to_parquet(input_file_path, output_file_path, drop_option, schema):
     df = pd.read_csv(input_file_path)
 
     if drop_option == 'row':
@@ -11,12 +14,13 @@ def convert_csv_to_parquet(input_file_path, output_file_path, drop_option):
     elif drop_option == 'column':
         df = df.dropna(axis=1)
 
-    df['before'] = df['before'].apply(lambda x: base64.b64encode(open(x, 'rb').read()).decode())
-    df['after'] = df['after'].apply(lambda x: base64.b64encode(open(x, 'rb').read()).decode())
+    df['before'] = df['before'].apply(lambda x: {'bytes': open(x, 'rb').read()})
+    df['after'] = df['after'].apply(lambda x: {'bytes': open(x, 'rb').read()})
 
-    table = pa.Table.from_pandas(df)
-    pq.write_table(table, output_file_path, compression='zstd')
+    table = pa.Table.from_pandas(df, schema=schema)
+    pq.write_table(table, output_file_path, compression='snappy')
     table = pq.read_table(output_file_path)
     df = table.to_pandas()
+    print(df.head())
 
-convert_csv_to_parquet('output.csv', 'data.parquet', 'row')
+convert_csv_to_parquet('output.csv', 'data.parquet', 'row', schema)
